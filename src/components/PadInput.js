a280abde6f43ca8b71af966799eb3d22d4b40c2d
Tahmina <tahmina.choudhury@lc.cuny.edu>
2021-10-29 21:07:53 -0400

    first commit

--- src/components/PadInput.js
+++ src/components/PadInput.js
@@ -4,7 +4,11 @@ class PadInput extends React.Component {
 
     render(){
         return(
-            <div>designpad</div>
+            <div>
+                background-color: lightblue;
+                border: 10px
+                opacity: 1.0
+            </div>
         )
     }
 }
