0732046a062e21ab69614edc85f7343d06e3709c
Tahmina <tahmina.choudhury@lc.cuny.edu>
2021-10-31 13:22:40 -0400

    first commit

--- src/components/PadInput.js
+++ src/components/PadInput.js
@@ -3,12 +3,22 @@ import React from 'react'
 class PadInput extends React.Component {
 
     render(){
-        return(
-            <div>
-                background-color: lightblue;
+       
+        return(   
+            
+            <><div>
+                1
+                backgroundColor: #999999;
                 border: 10px
                 opacity: 1.0
-            </div>
+
+            </div><div>
+                2
+                backgroundColor: #FFFFCC;
+                border: 10px
+                opacity: 1.0
+
+                </div></>
         )
     }
 }
